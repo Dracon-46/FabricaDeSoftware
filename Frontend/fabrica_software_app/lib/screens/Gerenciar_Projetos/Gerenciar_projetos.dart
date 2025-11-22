431bdac9ac513a66c6153e1b4fae36c24cf21451
Ronaldo Araujo Silva Junior <ronaldogamer2006@gmail.com>
2025-11-22 00:42:45 -0300

    finalizando personalização total do modal de criação,(não implementado cores, está com erro

--- Frontend/fabrica_software_app/lib/screens/Gerenciar_Projetos/Gerenciar_projetos.dart
+++ Frontend/fabrica_software_app/lib/screens/Gerenciar_Projetos/Gerenciar_projetos.dart
@@ -1,7 +1,11 @@
 import 'package:fabrica_software_app/Widgets/App_bar/App_bar.dart';
 import 'package:fabrica_software_app/Widgets/Barra_lateral/Barra_Lateral.dart';
+import 'package:fabrica_software_app/Widgets/Modal_de_criacao/Modal_de_criacao.dart';
+import 'package:fabrica_software_app/providers/modal_criacao_projeto_provider.dart';
 
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:provider/provider.dart';
 
 
 class GerenciarProjetos extends StatefulWidget {
@@ -17,6 +21,21 @@ class _GerenciarProjetosState extends State<GerenciarProjetos> {
     return Scaffold(
       appBar: CustomAppBar(title: 'Gestão de Projetos'),
       drawer: BarraLateral(),
+      body:ElevatedButton(onPressed: (){abrirModalCriacao(context);}, child: Text('Abrir modal')),
     );
   }
+}
+
+
+void abrirModalCriacao(BuildContext context) {
+  showDialog(
+    context: context,
+    barrierDismissible: false,
+    builder: (_) {
+      return ChangeNotifierProvider(
+        create: (_) => ModalCriacaoProjetoProvider(),
+        child: ModalDeCriacao(),
+      );
+    },
+  );
 }
